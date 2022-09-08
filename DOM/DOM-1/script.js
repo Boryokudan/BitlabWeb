df191a3ef92cd45b79cf670e4b4b72013a148293
ZYermukhamet <82998214+ZYermukhamet@users.noreply.github.com>
2022-09-08 16:19:43 +0600

    0809

---
+++ DOM/DOM-1/script.js
@@ -0,0 +1,10 @@
+let users = [
+    {"login": "ilyas", "password": "qwerty"},
+    {"login": "eldar", "password": "qqqqq"},
+    {"login": "assylkhan", "password": "qweqwe"},
+    {"login": "anel", "password": "asdasd"},
+    {"login": "alibek", "password": "aaaaaa"}
+];
+
+let login = document.getElementById("login").innerText;
+let password = document.getElementById("password").innerText;
