a08414cf7589f6d551ffb3dfca6680af0ddff865
ZYermukhamet <82998214+ZYermukhamet@users.noreply.github.com>
2022-09-08 17:26:03 +0600

    0809

--- DOM/DOM-1/script.js
+++ DOM/DOM-1/script.js
@@ -6,5 +6,35 @@ let users = [
     {"login": "alibek", "password": "aaaaaa"}
 ];
 
-let login = document.getElementById("login").innerText;
-let password = document.getElementById("password").innerText;
+function checkUser() {
+    let itemContainer1 = document.getElementById("itemContainer");
+    let success1 = document.getElementById("success");
+    let login1 = document.getElementById("login");
+    let password1 = document.getElementById("password");
+    let isValid = false;
+
+    while(!isValid) {
+        if (login1.innerText.localeCompare("") == 0 && password1.innerText.localeCompare("") == 0) {
+            alert("Login and password field cannot be empty!");
+            login1.style.border = "1px solid red";
+            password1.style.border = "1px solid red";
+        }
+        else if (login1.localeCompare("") == 0) {
+            alert("Login field cannot be empty!");
+            login1.style.border = "1px solid red";
+        }
+        else if (password1.localeCompare("") == 0) {
+            alert("Password field cannot be empty!");
+            password1.style.border = "1px solid red";
+        }
+        else isValid = true;
+    }
+
+    for (var i = 0; i < users.length; i++) {
+        if (users[i]["login"].localeCompare(login1.innerText) == 0 && users[i]["password"].localeCompare(password1.innerText) == 0) {
+            itemContainer1.style.display = "none";
+            success1.style.display = "flex";
+        }
+    }
+    
+}
